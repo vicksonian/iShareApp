d5bb2d4a3875e079ea89c710316b228c37644abc
George Michael Pussl <george.pussl@gmail.com>
2024-05-20 07:58:05 +0300

    WER34

--- test.js
+++ test.js
@@ -409,8 +409,8 @@ function fetchAndDisplayFiles() {
 		});
 }
 
-fetchAndDisplayFiles();
-const intervalId = setInterval(fetchAndDisplayFiles, 600000);
+// fetchAndDisplayFiles();
+// const intervalId = setInterval(fetchAndDisplayFiles, 600000);
 
 function truncateFileName(fileName, maxLength) {
 	if (fileName.length > maxLength) {
